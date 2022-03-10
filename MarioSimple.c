ad46f52a468074a4b6ab6ae94add7fa5494ee80c
TetianaSon <100778381+TetianaSon@users.noreply.github.com>
2022-03-10 17:27:55 +0200

    Create MarioSimple.c

---
+++ MarioSimple.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+int height = get_int ("Height: \n");
+printf("Height: %i\n", height);
+if (height >= 1 && height <= 8) 
+for (int i = 0; i<height+1; i++)
+{
+    for (int k = height-i; k>0; k--)
+    {
+        printf(" ");
+    }
+    for (int j = 0; j<i; j++)
+    {
+        printf("#");
+    }
+    printf("\n");
+}
+}
