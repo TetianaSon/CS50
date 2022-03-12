0e75cc83174cc2a61e234a7153a51686bd2703e8
TetianaSon <100778381+TetianaSon@users.noreply.github.com>
2022-03-12 03:14:20 +0200

    Update Lecture1MarioSimple.c

--- Lecture1MarioSimple.c
+++ Lecture1MarioSimple.c
@@ -1,21 +1,40 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int get_positive_int(string prompt)
+{
+int n;
+do
+{
+n = get_int("%s", prompt);
+}
+while ((n < 1) || (n > 8));
+return n;
+};
+
 int main(void)
 {
-int height = get_int ("Height: \n");
-printf("Height: %i\n", height);
+   int height = get_positive_int ("Height: ");
 if (height >= 1 && height <= 8) 
 for (int i = 0; i<height+1; i++)
 {
-    for (int k = height-i; k>0; k--)
+    if (i>=1)
+    {
+    if(i!=height)
+    {
+    for (int k = height-i; (k>=1); k--)
     {
         printf(" ");
+    }
     }
     for (int j = 0; j<i; j++)
     {
         printf("#");
     }
-    printf("\n");
+    if ((i>0) && (i<height+1))
+    {
+        printf("\n");
+    }
+}
 }
 }
